9c660e5f15173d3204439f9726251131bd3bc16a
DESKTOP-IFOLDOG\ryohe <yakigac@gmail.com>
2022-12-28 23:57:41 +0900

    リファクタリング実施

--- Code.js
+++ Code.js
@@ -21,17 +21,17 @@ function doPost(e) {
   }
 }
 
-function isDuplicateSlackEvent(slackEvent){
+function isDuplicateSlackEvent(slackEvent) {
   const channel = slackEvent.channel;
   const ts = slackEvent.ts;
   const cache = CacheService.getScriptCache();
 
   const cacheKey = channel + ':' + ts;
   const cached = cache.get(cacheKey); //同じcacheKeyでストアされているデータをゲットする（無ければNULLになる）
-  if(cached != null){
+  if (cached != null) {
     return true
   }
-  else{
+  else {
     cache.put(cacheKey, true, 60); // trueをcacheKeyでキャッシュする（単位sec）
   }
   return false
@@ -41,7 +41,7 @@ function handleEvent(event) {
   // Slackから送信されたイベントを取得する
   var slackEvent = event.event;
 
-  if(isDuplicateSlackEvent(slackEvent)){
+  if (isDuplicateSlackEvent(slackEvent)) {
     console.log('重複クエリのため、何もせず終了します。');
     return;
   }
@@ -59,15 +59,15 @@ function handleEvent(event) {
     // コマンドを判定する
     if (command == "/usage") {
       const usage = checkUsageThisMonth();
-      sendMessage("今月の使用量は$"+usage.current_usage_usd+"です。", slackEvent.channel);
+      sendMessage("今月の使用量は$" + usage.current_usage_usd + "です。", slackEvent.channel);
     }
-    else if(command == "/clear"){
+    else if (command == "/clear") {
       const cacheKey = slackEvent.channel;
       const cache = CacheService.getScriptCache();
       cache.remove(cacheKey); //同じcacheKeyでストアされているデータをゲットする（無ければNULLになる）
       sendMessage("記憶喪失しました。。", slackEvent.channel);
     }
-    else if(command == "/store"){
+    else if (command == "/store") {
       const channel = slackEvent.channel;
       const messages = getAndSetMessages(channel, null);
       const prompt = makePrompt(messages, "");
@@ -78,18 +78,18 @@ function handleEvent(event) {
 
       // promtを作成（同じチャンネルでの会話は10分以内なら覚えている）
       const cacheKey = slackEvent.channel;
-      const messages = getAndSetMessages(cacheKey, "Human:"+message);
+      const messages = getAndSetMessages(cacheKey, "Human:" + message);
 
       const prompt = makePrompt(messages);
       ai_message = getGpt3Message(prompt);
 
-      getAndSetMessages(cacheKey, "AI:"+ai_message);
+      getAndSetMessages(cacheKey, "AI:" + ai_message);
       sendMessage(ai_message, slackEvent.channel);
     }
-    else{
+    else {
       sendMessage("すみません、よくわからないです。。", slackEvent.channel);
     }
-    
+
   }
 }
 
@@ -124,7 +124,7 @@ function getGpt3Message(prompt) {
 
   // HTTPリクエストで使用するヘッダー
   var headers = {
-    'Authorization': 'Bearer '+ token, // APIキーを指定する
+    'Authorization': 'Bearer ' + token, // APIキーを指定する
     'Content-type': 'application/json' // データ形式を指定する
   };
 
@@ -137,27 +137,27 @@ function getGpt3Message(prompt) {
     // 生成される文章の最大トークン数を指定。単語数というような意味
     // 1000辺り$0.02なので少なくしておく
     max_tokens: 500,
-      // 0.5と指定すると生成される文章は入力となる文章に似たものが多くなる傾向があります。
-      // 逆に、temperatureフィールドに1.0と指定すると、生成される文章は、より多様なものになる傾向があります。
+    // 0.5と指定すると生成される文章は入力となる文章に似たものが多くなる傾向があります。
+    // 逆に、temperatureフィールドに1.0と指定すると、生成される文章は、より多様なものになる傾向があります。
     temperature: 0.5,
-    stop:["\nAI:","\nHuman:"]
+    stop: ["\nAI:", "\nHuman:"]
   };
   // HTTPリクエストで使用するオプション
   var options = {
-    'muteHttpExceptions' : true, // HTTPエラーを無視する
+    'muteHttpExceptions': true, // HTTPエラーを無視する
     'headers': headers, // ヘッダーを指定する
     'method': 'POST', // HTTPメソッドを指定する
     'payload': JSON.stringify(requestBody),// リクエストボディを指定する
   };
   try {
-      // GPT-3にリクエストを送信する
-      const response = UrlFetchApp.fetch(uri, options);
-      // レスポンスを取得する
-      var json=JSON.parse(response.getContentText());
-      // GPT-3からのレスポンスを返す
-      const response_text = json["choices"][0]["text"];
-      return response_text;
-  } catch(e) {
+    // GPT-3にリクエストを送信する
+    const response = UrlFetchApp.fetch(uri, options);
+    // レスポンスを取得する
+    var json = JSON.parse(response.getContentText());
+    // GPT-3からのレスポンスを返す
+    const response_text = json["choices"][0]["text"];
+    return response_text;
+  } catch (e) {
     console.error(e);
   }
 }
@@ -202,26 +202,26 @@ function checkUsageThisMonth() {
   return usage
 }
 
-function makePrompt(messages, aiPrefix="AI:"){
+function makePrompt(messages, aiPrefix = "AI:") {
   var prompt = `以下はAIアシスタントとの対話です。アシスタントは簡潔に受け答えします。AIは以下ルールを守ります。
   1. プログラムを記載する際はslackのコードブロックで囲む。
   2. 回答は原則140文字以内に収める。もし140文字以内で回答しきれない場合は改行後に「続けてもよろしいでしょうか？」というメッセージで問いかけを行い、Humanの返答を待つ。
   3. Humanが明示的に許可している場合、2の140文字ではなく、500文字以内で回答する。\n`
-  + messages.join('\n')
-  + `\n`
-  + aiPrefix;
+    + messages.join('\n')
+    + `\n`
+    + aiPrefix;
 
   return prompt
 }
 
-function getAndSetMessages(cacheKey, newMessage){
+function getAndSetMessages(cacheKey, newMessage) {
   const cache = CacheService.getScriptCache();
   const prevMessagesString = cache.get(cacheKey); //同じcacheKeyでストアされているデータをゲットする（無ければNULLになる）
   var messages = JSON.parse(prevMessagesString);
-  if(messages == null){
+  if (messages == null) {
     messages = [];
   }
-  if(newMessage!=null){
+  if (newMessage != null) {
     messages.push(newMessage)
   }
 
@@ -231,33 +231,35 @@ function getAndSetMessages(cacheKey, newMessage){
   return messages
 }
 
-function postMessageSnippet(message, channel){
-  // Slack APIのトークンを取得する
+function postMessageSnippet(message, channel) {
+  // Slack APIトークンをスクリプトプロパティから取得する
   const token = PropertiesService.getScriptProperties().getProperty("SLACK_TOKEN");
+  // Slack APIのfiles.uploadエンドポイント
+  const SLACK_API_ENDPOINT = "https://slack.com/api/files.upload";
 
-  //Jsonのペイロードに格納  
+  // Slack APIのfiles.uploadエンドポイントを使用して、投稿するテキストファイルのコンテンツを設定する
   var payload = {
-    "token" : token,
-    "channels" : channel,
-    'content' : message,//メッセージの中身
-    'filename':"prompt.txt",//テキスト形式のファイルを指定
-    //'initial_comment': msgFrom,//ファイルのコメント"
-    'title': "Messages" //"Slack上でのファイルのタイトル"
-  };
-  var options = {
-    "method" : "post",
-    "headers": {
-      "Authorization": "Bearer " + token
-    },
-    'contentType': 'application/x-www-form-urlencoded',
-    "payload" : payload
+    "token": token, // Slack APIトークン
+    "channels": channel, // 投稿先のチャンネルID
+    'content': message, // メッセージの中身
+    'filename': "prompt.txt", // テキスト形式のファイルを指定
+    'title': "Messages" // Slack上でのファイルのタイトル
   };
 
-  const uri = "https://slack.com/api/files.upload"
+  // Slack APIへのリクエストで使用するオプションを設定する
+  var options = {
+    "method": "post", // HTTPのPOSTメソッドを使用する
+    "headers": {
+      "Authorization": "Bearer " + token // Slack APIトークンを使用する
+    },
+    'contentType': 'application/x-www-form-urlencoded', // コンテンツタイプを指定する
+    "payload": payload // リクエストペイロードを設定する
+  };
+
   try {
     // Postリクエストを送信する
-    UrlFetchApp.fetch(uri, options);
-  } catch(e) {
-    console.error(e);
+    UrlFetchApp.fetch(SLACK_API_ENDPOINT, options);
+  } catch (e) {
+    console.error(e); // エラーが発生した場合は、コンソールにエラーを出力する
   }
 }
